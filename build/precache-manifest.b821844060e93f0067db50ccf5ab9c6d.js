429f9d23db46eae1f69e454b6afd048f488dead2
norman-anderson <thenormanhimself@gmail.com>
2023-08-29 16:28:17 -0400

    React build

---
+++ build/precache-manifest.b821844060e93f0067db50ccf5ab9c6d.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c96c12b99993d2c47d5bed11ad6626be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4846aa0a755808104cc0",
+    "url": "/static/css/main.48e1f9ac.chunk.css"
+  },
+  {
+    "revision": "1f7ba63ac269222d4487",
+    "url": "/static/js/2.6e419bb0.chunk.js"
+  },
+  {
+    "revision": "0aadc8a8405aeed76ae7e020b1d921e7",
+    "url": "/static/js/2.6e419bb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4846aa0a755808104cc0",
+    "url": "/static/js/main.5f6bc28c.chunk.js"
+  },
+  {
+    "revision": "ad3971d54a6c92ea04ed",
+    "url": "/static/js/runtime-main.09ceff5b.js"
+  },
+  {
+    "revision": "b30698fd407f815496889ccfb376ca1a",
+    "url": "/static/media/arrow-dark.b30698fd.svg"
+  },
+  {
+    "revision": "873e41507cf45c1984c3cef30b76e867",
+    "url": "/static/media/arrow-light.873e4150.svg"
+  },
+  {
+    "revision": "c145e541b3b5e803018950853a720cdd",
+    "url": "/static/media/carousel-arrow-dark.c145e541.svg"
+  },
+  {
+    "revision": "45b011613fc252a301fdb9428f450aa5",
+    "url": "/static/media/carousel-arrow-light.45b01161.svg"
+  },
+  {
+    "revision": "18d9217b5751fdbba0c1bca5d7df0d7b",
+    "url": "/static/media/cursor.18d9217b.svg"
+  },
+  {
+    "revision": "c26549a4dad29dc154ed9833cb061421",
+    "url": "/static/media/cursor.c26549a4.cur"
+  },
+  {
+    "revision": "7c64de5df15fa80f6977a31792c0a559",
+    "url": "/static/media/linkArrow.7c64de5d.svg"
+  },
+  {
+    "revision": "5ce4bfd1b867066fe1d2a74a372fc261",
+    "url": "/static/media/norman-pic.5ce4bfd1.jpeg"
+  },
+  {
+    "revision": "ace0cb74ec68fb3b716a60b0d6f6aeda",
+    "url": "/static/media/painting.ace0cb74.JPG"
+  },
+  {
+    "revision": "65a7afe8a23a242ea975706143803404",
+    "url": "/static/media/saona.65a7afe8.jpg"
+  },
+  {
+    "revision": "532c56501ec4a751b143946f88857fca",
+    "url": "/static/media/singing.532c5650.jpg"
+  }
+]);
